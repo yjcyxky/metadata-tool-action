8ebd2e67b060b159f45daebe7702bbe6d671d71e
YJC <yjcyxky@163.com>
2022-04-04 20:19:53 +0800

    Improve entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,18 +13,19 @@ export TEST_MODE=$2
 COMMAND="java -jar /opt/metadata-tool.jar -d ${DATA_DIR} -m -n -D"
 echo "Runing command: ${COMMAND}"
 
+echo "DB_PASSWD: ${DB_PASSWD}, AUTH_KEY: ${AUTH_KEY}, AUTH_VALUE: ${AUTH_VALUE}"
+echo "DINGTALK_ACCESS_TOKEN: ${DINGTALK_ACCESS_TOKEN}, DINGTALK_ACCESS_SECRET: ${DINGTALK_ACCESS_SECRET}"
+
 if [ "$TEST_MODE" == 'false' ]; then
     if [ ! -f "$DATA_DIR/env.sh" ];then
         echo "ERROR: not found env.sh in the data directory"
         exit 1
     else
         echo "Set environment variables in $DATA_DIR/env.sh..."
+        cat $DATA_DIR/env.sh
         source $DATA_DIR/env.sh
         java -jar /opt/metadata-tool.jar -d ${DATA_DIR} -m -n -D
     fi
-else
-    echo "DB_PASSWD: ${DB_PASSWD}, AUTH_KEY: ${AUTH_KEY}, AUTH_VALUE: ${AUTH_VALUE}"
-    echo "DINGTALK_ACCESS_TOKEN: ${DINGTALK_ACCESS_TOKEN}, DINGTALK_ACCESS_SECRET: ${DINGTALK_ACCESS_SECRET}"
 fi
 
 echo ::set-output name=command::$COMMAND
